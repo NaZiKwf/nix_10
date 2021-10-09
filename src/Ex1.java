acb354bced9daac92e84b1405222e5e490d1fad6
NaZiKwf <nazar.akolovich@gmail.com>
2021-10-09 17:03:15 +0300

    unit_1

--- src/Ex1.java
+++ src/Ex1.java
@@ -3,31 +3,39 @@ import java.util.Scanner;
 public class Ex1
 {
     public static void main(String[] args){
+
+        Ex1();
+    }
+    public static void Ex1() {
         Scanner in=new Scanner(System.in);
         System.out.println("Enter");
+        String str;
         int sum=0;
+        str= in.nextLine();
+        for(int i=0;i<str.length();i++){
+            try{
+                sum+=Integer.parseInt(str.substring(i,i+1));
 
-        while(true){
-            if(in.hasNextLine()){
-                in.nextLine();
-                while(true) {
-                    int temp;
-
-                    try{
-                        temp = in.nextInt();
-                        sum+=temp;
-                    System.out.println("YA ZDES`");
-                    }
-                    catch (Exception e){
-                        break;
-                    }
-
-                }
-                break;
-            }
-
+            }catch (Exception ignored){}
         }
 
         System.out.println(sum);
+
     }
+    public static void Ex2(){
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter");
+        String str;
+        int sum=0;
+        str=in.nextLine();
+        for(int i=0;i<str.length();i++){
+
+        }
+
+
+
+    }
+
 }
+
+
