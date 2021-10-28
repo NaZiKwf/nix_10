616cc8e05b65f89d9f20df37284abb6b7ffdc871
NaZiKwf <92051663+NaZiKwf@users.noreply.github.com>
2021-10-28 12:33:07 +0300

    Update

--- module_1/src/main/java/ua/com/alevel/Main.java
+++ module_1/src/main/java/ua/com/alevel/Main.java
@@ -4,15 +4,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //System.out.println("Enter string with numbers");
         Lvl1Task1 Task1 = new Lvl1Task1();
-        Task1.main (null);
-
-        //System.out.println("Enter string");
+        Task1.main(null);
         Lvl1Task2 Task2 = new Lvl1Task2();
         Task2.main(null);
-
-        //System.out.println("Enter number of lesson");
         Lvl1Task3 Task3 = new Lvl1Task3();
         Task3.main(null);
     }
