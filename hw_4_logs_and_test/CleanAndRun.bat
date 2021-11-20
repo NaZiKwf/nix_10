f5724ddf5fafce074e994d3f673e56dbde5eee9e
egorfuntusov <funtushan@gmail.com>
2021-11-20 08:45:18 +0200

    add review

--- hw_4_logs_and_test/CleanAndRun.bat
+++ hw_4_logs_and_test/CleanAndRun.bat
@@ -1,2 +1,2 @@
 call mvn clean install
-call java -jar hw_4_logs_and_test/target/hw_4_logs_and_test.jar
+call java -jar ./target/hw_4_logs_and_test.jar
--- hw_4_logs_and_test/Start.bat
+++ hw_4_logs_and_test/Start.bat
@@ -1,1 +1,1 @@
-call java -jar hw_4_logs_and_test/target/hw_4_logs_and_test.jar
+call java -jar ./target/hw_4_logs_and_test.jar
